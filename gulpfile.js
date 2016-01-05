879dd234735343e934782cacfd6a0c956fbd908d
Anton Panchenko <anton.panchenko@formpipe.com>
2016-01-05 15:05:59 +0200

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    minifyCSS = require('gulp-minify-css'),
+    cssAutoPrefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+  gulp.src('./sass/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssAutoPrefixer('last 5 versions'))
+    .pipe(minifyCSS(''))
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch:sass', function () {
+  gulp.watch('./sass/styles.scss', ['sass']);
+});
+
+gulp.task('run', ['sass', 'watch:sass']); // default task
