80dc224b8930e32f943f76a5e1b76defc74a07ec
Anton Panchenko <anton.panchenko@formpipe.com>
2016-02-02 18:12:47 +0200

    select all

--- js/controllers/FmContainerCtrl.js
+++ js/controllers/FmContainerCtrl.js
@@ -1,11 +1,12 @@
 define(['app', '../services/API', '../directives/fm-layout-table', '../directives/fm-layout-tile'], function (app) {
-    app.controller('FmContainerCtrl', ['$scope', 'api', 'localStorageService', '$rootScope',
-        function ($scope, api, localStorageService, $rootScope) {
+    app.controller('FmContainerCtrl', ['$scope', '$filter', 'api', 'localStorageService', '$rootScope',
+        function ($scope, $filter, api, localStorageService, $rootScope) {
 
             $scope.menuFoldersTree = '/partials/menu-folder-tree.html';
             $scope.contextMenuState = false;
+            $scope.disabledMenuItem = true;
+            $scope.disabledPaste = true;
             $scope.allSelected = false;
-            $scope.breadcrumbArr = [];
 
             api.getJSONresponse('foldersTree').then(function (data) {
                 $scope.foldersTree = data;
@@ -23,9 +24,12 @@ define(['app', '../services/API', '../directives/fm-layout-table', '../directive
             $scope.breadcrumbWalker = function (index, last) {
                 if (last) return;
                 var destArr = $scope.breadcrumbArr.slice(0, index + 1);
+
+                //======================================
                 for (var i = 0; i < destArr.length; i++) {
-                    $scope.tableData
+                    $scope.tableData;
                 }
+                //======================================
             };
 
             $scope.toggleLevelBelow = function (obj) {
@@ -46,13 +50,18 @@ define(['app', '../services/API', '../directives/fm-layout-table', '../directive
 
             $scope.selectAll = function (data) {
                 for (var i = 0; i < data.length; i++) {
-                    data[i]['selected'] = !$scope.allSelected;
+                    data[i].selected = !$scope.allSelected;
                 }
                 $scope.allSelected = !$scope.allSelected;
+                $scope.disabledMenuItem = !$scope.allSelected;
             };
 
             $scope.selectThis = function (item) {
                 item.selected = !item.selected;
+                var arr = $filter('filter')($scope.tableData, { selected: true });
+                $scope.disabledMenuItem = (arr.length <= 0) ? true : false;
+                if (arr.length <= 0) $scope.allSelected = false;
+                if (arr.length == $scope.tableData.length) $scope.allSelected = true;
             };
 
             $scope.getDocSize = function (fSize) {
