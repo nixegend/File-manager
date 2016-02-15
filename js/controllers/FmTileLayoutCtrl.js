1e7911ac9dfb9f5e1780c72285167c97c67667ae
Anton Panchenko <anton.panchenko@formpipe.com>
2016-02-15 19:07:02 +0200

    tile styles

--- js/controllers/FmTileLayoutCtrl.js
+++ js/controllers/FmTileLayoutCtrl.js
@@ -2,5 +2,19 @@ define(['app'], function (app) {
     app.controller('FmTileLayoutCtrl', ['$scope', '$rootScope', 'localStorageService',
         function ($scope, $rootScope, localStorageService) {
 
+            $scope.setfileClass = function (ext) {
+                switch (ext) {
+                    case 'txt': ''; break;
+                    case '': ''; break;
+                    case '': ''; break;
+                    case '': ''; break;
+                    case '': ''; break;
+                    case '': ''; break;
+
+                    default: 'fa-file-text-o'; break;
+                }
+            };
+
+
         }]);
 });
